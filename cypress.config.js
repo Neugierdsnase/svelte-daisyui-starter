0f2810a9e41d3b5818bf3afeff00bb352cb6c8e6
Konsti K <mail@vomkonstant.in>
2022-06-10 12:06:27 +0000

    configured cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+	env: {
+		base_url: 'http://localhost:3000'
+	},
+	video: false,
 	e2e: {
-		setupNodeEvents(on, config) {
-			// implement node event listeners here
-		}
+		// setupNodeEvents(on, config) {
+		// 	// implement node event listeners here
+		// }
 	}
 });
