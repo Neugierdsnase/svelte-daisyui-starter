0e1face28b73768b7635d7f0161025ed3977b8f3
Konsti K <mail@vomkonstant.in>
2022-06-10 15:15:09 +0000

    added programmatic navigation

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { NavItemType } from '../types/navItem';
+	import { fly } from 'svelte/transition';
+
+	export let menu: NavItemType[];
+</script>
+
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<a href="/" class="btn btn-ghost text-xl normal-case">daisyUI</a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal p-0">
+			{#each menu as { href, label }, index}
+				<li in:fly={{ y: -22, duration: 200, delay: 200 * index }}>
+					<a {href}>{label}</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,35 @@
-<script>
-	import '../app.css';
+<script lang="ts" context="module">
+	import type { NavItemType } from './../types/navItem';
+
+	const modules = import.meta.glob('../routes/**.svelte');
+	let fullMenu: NavItemType[] = [];
+
+	for (let path in modules) {
+		const label = path
+			.replace(/^\..\/routes\//, '')
+			.replace(/\.svelte$/, '');
+		if (!(label === 'index' || label.startsWith('__'))) {
+			fullMenu.push({
+				label,
+				href: `/${label}`
+			});
+		}
+	}
+
+	export const load = async () => {
+		const menu = await Promise.all(fullMenu);
+		return { props: { menu } };
+	};
 </script>
 
-<slot />
+<script lang="ts">
+	import Navbar from '../components/Navbar.svelte';
+	import '../app.css';
+
+	export let menu: NavItemType[];
+</script>
+
+<Navbar {menu} />
+<main class="mx-auto w-2/3 text-center text-6xl font-bold text-red-800">
+	<slot />
+</main>
